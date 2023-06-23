1ce4d3d430567724d8ccd043f0842d796c3f11b0
Alessandro de Oliveira <Alessandro1395271@outlook.com>
2023-06-22 21:55:27 -0300

    Possible moves of a piece

--- src/BoardGame/Piece.java
+++ src/BoardGame/Piece.java
@@ -1,21 +1,36 @@
 package BoardGame;
 
-public class Piece {
+public abstract class Piece {
 
 	protected Position position;
-	
+
 	private Board board;
 
 	public Piece(Board board) {
 		this.board = board;
-		position = null; //A posição inicial sempre será nula
+		position = null; // A posição inicial sempre será nula
 	}
 
 	protected Board getBoard() {
 		return board;
 	}
 
+	public abstract boolean[][] possibleMoves();
 
-	
-	
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
+
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] mat = possibleMoves();
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat.length; j++) {
+				if(mat[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
 }
--- src/Chess/piece/King.java
+++ src/Chess/piece/King.java
@@ -4,7 +4,7 @@ import BoardGame.Board;
 import Chess.ChessPiece;
 import Chess.Color;
 
-public class King extends ChessPiece{
+public class King extends ChessPiece {
 
 	public King(Board board, Color color) {
 		super(board, color);
@@ -14,5 +14,11 @@ public class King extends ChessPiece{
 	public String toString() {
 		return "K";
 	}
-	
+
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		return mat;
+	}
+
 }
